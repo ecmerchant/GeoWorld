651c86f74e1b4d448076d713fc5482513fade5c5
ecmerchant <ecmerchant.contact@gmail.com>
2019-03-06 23:33:22 +0900

    upd

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -14,7 +14,7 @@ class SchedulesController < ApplicationController
       for int in 1..10 do
         logger.debug(int)
         logger.debug(stime[int.to_s].values)
-        if ltype[int.to_s] != "" && ltype[int.to_s] != nil then
+        #if ltype[int.to_s] != "" && ltype[int.to_s] != nil then
           tag = @schedules.find_or_create_by(
             schedule_num: int
           )
@@ -24,7 +24,7 @@ class SchedulesController < ApplicationController
             schedule_type: stype[int.to_s],
             set_time: Time.new(tt[0].to_i, tt[1].to_i, tt[2].to_i, tt[3].to_i, tt[4].to_i)
           )
-        end
+        #end
       end
       redirect_to schedules_setup_path
     end
